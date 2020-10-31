8b933e4f4993af2c5d025c070b1a0943d39285d7
Thy Christiansen <thyvu0411@gmail.com>
2020-10-31 15:15:46 -0500

    created event for buttons

--- src/Calculator/Calculator.js
+++ src/Calculator/Calculator.js
@@ -12,38 +12,52 @@ class Calculator extends Component {
         isOperatorChosen: false,
         isCalculated: false,
     }
+
+    handleNumberClick(event){
+        this.setState({
+            firstNumber: event.target.innerText
+        })
+        // console.log(event.target.innerText);
+    }
+
+    handleOperatorClick(event){
+        this.setState({
+            operator: event.target.innerText
+        })
+        console.log(event.target.innerText);
+    }
     render() {
         return (
             <div class="col-lg-6">
                 <div class="calculator_body">
-                    <button class="button number">1</button>
-                    <button class="button number">2</button>
-                    <button class="button number">3</button>
-                    <button class="button operate">+</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>1</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>2</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>3</button>
+                    <button class="button operate" onClick={(value)=>this.handleOperatorClick(value)}>+</button>
                     <br />
-                    <button class="button number">4</button>
-                    <button class="button number" >5</button>
-                    <button class="button number">6</button>
-                    <button class="button number">-</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>4</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>5</button>
+                    <button class="button number"onClick={(value)=>this.handleNumberClick(value)}>6</button>
+                    <button class="button number" onClick={(value)=>this.handleOperatorClick(value)}>-</button>
                     <br />
-                    <button class="button number">7</button>
-                    <button class="button number">8</button>
-                    <button class="button number">9</button>
-                    <button class="button number">x</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>7</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>8</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>9</button>
+                    <button class="button number" onClick={(value)=>this.handleOperatorClick(value)}>x</button>
                     <br />
-                    <button class="button number">0</button>
-                    <button class="button number">:</button>
-                    <button class="button number">C</button>
-                    <button class="button number">=</button>
+                    <button class="button number" onClick={(value)=>this.handleNumberClick(value)}>0</button>
+                    <button class="button number" onClick={(value)=>this.handleOperatorClick(value)} >:</button>
+                    <button class="button number" onClick={(value)=>this.handleOperatorClick(value)}>C</button>
+                    <button class="button number" onClick={(value)=>this.handleOperatorClick(value)}>=</button>
                 </div>
 
 
                 <div class="col-lg-6 ">
                     <div class="card display-calculation">
                         <div class="card-body">
-                            <p id="first-number">1</p>
-                            <p id="operator">+</p>
-                            <p id="second-number">1</p>
+                            <p id="first-number">{this.state.firstNumber}</p>
+                            <p id="operator">{this.state.operator}</p>
+                            <p id="second-number">{this.state.secondNumber}</p>
                             <p>{this.state.result}</p>
                         </div>
                     </div>
@@ -58,12 +72,9 @@ class Calculator extends Component {
                             <li>1+1= 2</li>
                             <li>1+1= 2</li>
                             <li>1+1= 2</li>
-
                         </ul>
                     </div>
                 </div>
-
-
             </div >
         )
     }
