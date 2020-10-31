4af310520b167214ec1f2ac78232cb1c8f807f36
Thy Christiansen <thyvu0411@gmail.com>
2020-10-31 17:41:02 -0500

    get top 10 calculations

--- server/routes/calculator.router.js
+++ server/routes/calculator.router.js
@@ -3,16 +3,20 @@ const Calculation = require("../models/calculator");
 
 module.exports = function (app) {
   app.get("/api/all", function (req, res) {
-    Calculation.findAll({}).then(function (results) {
+    Calculation.findAll({
+      limit: 10,
+      order: [["id", "DESC"]]
+    }).then(function (results) {
       res.json(results);
     });
   });
 
   app.post("/api/calculation", function (req, res) {
-    console.log("Calculation Data:",req.body);
-    const {firstNumber, secondNumber, operator,result} = req.body
+    console.log("Calculation Data:", req.body);
+    const { firstNumber, secondNumber, operator, result } = req.body
     Calculation.create(
-      {first_number: firstNumber,
+      {
+        first_number: firstNumber,
         second_number: secondNumber,
         operator: operator,
         result: result
