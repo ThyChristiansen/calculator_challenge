629df3477c6f94e3d8cf025ab67928d6e5406e28
Thy Christiansen <thyvu0411@gmail.com>
2020-11-03 17:10:45 -0600

    Wiring app with JawsDB

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,20 +1,27 @@
 //Connecting to Mysql
+
 const MYSQLPASSWORD = process.env.MYSQLPASSWORD;
 
 // Dependencies
 const Sequelize = require("sequelize");
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-const sequelize = new Sequelize("calculator_sequelize", "root", MYSQLPASSWORD, {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  }
-});
+let sequelize;
+if (process.env.JAWSDB_URL) {
+   sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
+   sequelize = new Sequelize("calculator_sequelize", "root", MYSQLPASSWORD, {
+    host: "localhost",
+    port: 3306,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      idle: 10000
+    }
+  });
+}
+
 
 // Exports the connection for other files to use
 module.exports = sequelize;
